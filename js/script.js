ce5233e2a3696e5abd5f53a7fc750a1afae5217b
NicholasBuiatti <nic.buiatti@gmail.com>
2024-04-29 14:51:19 +0200

    m-2 (palindromo) creazione funzione per qualsiasi caso

--- js/script.js
+++ js/script.js
@@ -4,25 +4,42 @@
 let word = 'anna';
 // console.log(word[0]);
 
-let wordReverse = '';
-for (let i = word.length - 1; i >= 0; i--) {
-    const element = word[i];
-    console.log(element);
-    wordReverse += word[i];
-    console.log(wordReverse);
-}
+// let wordReverse = '';
+// for (let i = word.length - 1; i >= 0; i--) {
+//     const element = word[i];
+//     console.log(element);
+//     wordReverse += word[i];
+//     console.log(wordReverse);
+// }
 
-if (word === wordReverse) {
-    console.log('Palindroma')
-} else {
-    console.log('Non Palindroma');
-}
-
-
-
+// if (word === wordReverse) {
+//     console.log('Palindroma')
+// } else {
+//     console.log('Non Palindroma');
+// }
 
 // Creare una funzione per capire se la parola inserita è palindroma.
+function palindromaOrNot(parola) {
+    
+    //la parola/frase al contrario
+    let wordReverse = '';
+    
+    for (let i = parola.length - 1; i >= 0; i--) {
+        const element = parola[i];
+        // console.log(element);
+        wordReverse += parola[i];
+        // console.log(wordReverse);
+    }
 
+    //controllo che la parola/frase sia uguale a quella al contrario
+    if (parola === wordReverse) {
+        return ('Palindroma')
+    } else {
+        return ('Non Palindroma');
+    }
+}
+
+console.log(palindromaOrNot(word))
 
 // BONUS uso del ciclo.
 // (Eventuali soluzioni più "comode" potete aggiungere come extra in un commit separato solo DOPO.)
